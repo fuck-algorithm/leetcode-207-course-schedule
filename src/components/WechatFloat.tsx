fdce49e5a29cc7ede2d32994c434869da3e1428f
CC11001100 <CC11001100@qq.com>
2025-12-18 17:31:32 +0800

    fix: 悬浮球显示'交流群'文字

--- src/components/WechatFloat.tsx
+++ src/components/WechatFloat.tsx
@@ -11,9 +11,7 @@ export default function WechatFloat() {
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="wechat-icon">
-        <svg viewBox="0 0 24 24" width="28" height="28" fill="white">
-          <path d="M8.5 11.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm4 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm-6.5 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm4 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2zM9 3C4.58 3 1 6.13 1 10c0 2.17 1.16 4.1 3 5.38V19l3.5-2.1c.5.07 1 .1 1.5.1 4.42 0 8-3.13 8-7s-3.58-7-8-7zm6.5 16.5l2.5 1.5v-2.62c1.84-1.28 3-3.21 3-5.38 0-2.17-1.16-4.1-3-5.38V5c2.76 1.5 4.5 4.5 4.5 8 0 5-4.03 9-9 9-.5 0-1-.03-1.5-.1l3-1.9z"/>
-        </svg>
+        <span className="wechat-icon-text">交流群</span>
       </div>
       {isHovered && (
         <div className="wechat-popup">
